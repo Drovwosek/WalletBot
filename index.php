da539bd7dde7719b4cf7e3781ed2e5fd3c1fab51
Drovwosek <61798378+Drovwosek@users.noreply.github.com>
2020-08-02 10:10:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,14 @@
 require_once 'vendor/autoload.php';
 use Telegram\Bot\Api;
 
-$token = "805768483:AAEq-Nice09ubQ3bLiRr3ir82piq59uHK2Y";
-$bot = new \TelegramBot\Api\Client($token);
-// команда для start
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
+$bot->command('hello', function ($message) use ($bot) {
+    $text = $message->getText();
+    $param = str_replace('/hello ', '', $text);
+    $answer = 'Неизвестная команда';
+    if (!empty($param))
+    {
+    	$answer = 'Привет, ' . $param;
+    }
     $bot->sendMessage($message->getChat()->getId(), $answer);
 });
 
-// команда для помощи
-$bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - вывод справки';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
-
-$bot->run();
